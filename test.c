88abee34ae414421d8cce99b6154cdac2f86c7d0
Paolo Rampino <liadserv@gmail.com>
2024-06-11 19:23:25 +0200

    up

--- test.c
+++ test.c
@@ -1,8 +1,33 @@
 #include <stdio.h>
-#define _USE_MATH_DEFINES
-#include <math.h>
 
-int main(){
-    printf("Che ti ridi!\n\n");
+void scrivivettore(int *ptr)
+{
+    for (int i = 0; i < 10; i++)
+    {
+        printf("\ninserisci un numero: ");
+        scanf("%d", ptr);
+        ptr++;
+    }
+}
+
+void leggivettore(int *ptr)
+{
+    for (int i = 0; i < 10; i++)
+    {
+        printf("\ni=%d 0x%x=%d", i, ptr, *ptr);
+        ptr++;
+    }
+}
+
+int main()
+{
+    int n[10] = {0};
+    int *n_ptr;
+
+    n_ptr = &n[0];
+
+    scrivivettore(n_ptr);
+    leggivettore(n_ptr);
+
     return 0;
 }
